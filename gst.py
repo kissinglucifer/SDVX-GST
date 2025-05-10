bf8d0fb6c2678cceb28bc36a72ac554d5479a0d3
koid <70305334+kissinglucifer@users.noreply.github.com>
2025-05-10 16:46:51 -0600

    add date before date and defaults to overwrite
    i did it mom!! you proud!! i fixed the thing i fucked up!!

--- gst.py
+++ gst.py
@@ -14,18 +14,19 @@ parser = argparse.ArgumentParser(prog='gst')
 parser.add_argument('-i input_folder', dest='input', help='Path to contents folder. This is the folder containing data\\.', required=True)
 parser.add_argument('-o output_folder', dest='output', help='Path to output folder. This is where the GST will be.', required=True)
 parser.add_argument('-v game_ver', dest='version', type=int, help='Generate GST for only one version. Leave blank to generate full GST.')
-parser.add_argument('-d after_date', dest='date', type=int, help='Only add songs added past this date as YYYYMMDD. Defaults to 0.', default=0)
+parser.add_argument('-d after_date', dest='after_date', type=int, help='Only add songs added after this date as YYYYMMDD. Defaults to 0.', default=0)
+parser.add_argument('-b before_date', dest='before_date', type=int, help='Only add songs added before this date as YYYYMMDD. Defaults to 0.', default=0)
 parser.add_argument('-y', '--youtube', dest='yt', action='store_true', help='Save GST as MP4 files for YouTube uploading.')
-parser.add_argument('-vb', '--verbose', dest='verbose', action='store_true', help='Verbose ffmpeg output. Disables progress bar')
+parser.add_argument('-vb', '--verbose', dest='verbose', action='store_true', help='Verbose ffmpeg output. \\Disables progress bar')
 parser.add_argument('-j job', dest='job', type=int, help='Number of jobs active at once (cpu dependent). Defaults to 2.', default=2)
-
-
 args = parser.parse_args()
 
 in_path = Path(args.input)
 out_path = Path(args.output)
 target_version = args.version
-target_date = args.date
+after_date = args.after_date
+before_date = args.before_date
+
 as_video = args.yt
 if args.verbose:
     loglevel = "info"
@@ -132,8 +133,10 @@ def parse_mdb(musicdb):
         if target_version and version != target_version:  # If getting one version
             continue
         release_date = int(info.find('distribution_date').text)
-        if release_date < target_date: # If getting after date
-            continue
+        
+        if after_date != 0 or before_date != 0:
+            if release_date < after_date or release_date > before_date: # If getting after date
+                continue
         title = info.find('title_name').text
         artist = info.find('artist_name').text
         for text, accent in accent_decode.items():
@@ -173,7 +176,7 @@ def add_song(song):
             (
                 ffmpeg
                 .output(main, cover, f'{out_path}/{sani_artist} - {sani_title}{diff_abb}.mp4', acodec='aac', vcodec='libx264', ab='256k', pix_fmt='yuv420p', loglevel=loglevel)
-                .run()
+                .run(overwrite_output=True)
             )
             return
         
@@ -182,7 +185,7 @@ def add_song(song):
             ffmpeg
             .input(s3v_file)
             .output(mp3_file, loglevel=loglevel)
-            .run()
+            .run(overwrite_output=True)
         )
         
         song_file = music_tag.load_file(mp3_file)
